298e8daa3d7135290618ecdbca040c5c441b72c3
Jhonatan-Jeferson <jhonatanjefersonoliveira@gmail.com>
2025-11-04 14:15:24 -0300

    fix(example/upload_file.py): Fixing upload example

--- examples/upload_file.py
+++ examples/upload_file.py
@@ -11,5 +11,8 @@ async def main():
     # in this example i will pass the kyojuro_rengoku.jpg 
     # it is in examples folder
     # and it name will be my_image
-    uploaded_object = await blob_client.upload_object("examples/kyojuro_rengoku.jpg", "my_image")
+    uploaded_object = await blob_client.upload_object(file='examples/kyojuro_rengoku.jpg', name="my_image_rengoku")
     print(uploaded_object)
+
+    # OBS.: You can also pass a bytes, bytesIO or BufferedIOBase object instead of a path string
+    # You'll need to pass the mimetype in this case. The library will try to guess the mimetype but it's not guaranteed to work always
